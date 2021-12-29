c5f9a924d1adc5c6d58a6d2c0e387d6216ad8e10
Niall Flanagan <niall.flanagan@lacework.net>
2021-12-29 23:43:07 +0000

    feat(sensor) added logic for ds18b20 sensor
    Signed-off-by: Niall Flanagan <niall.flanagan@lacework.net>

--- src/monitor/sensors.py
+++ src/monitor/sensors.py
@@ -1,8 +1,23 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 from enum import Enum, auto
+from pathlib import Path
 
 from monitor.measurements import Measurement
+
+
+class SensorInitError(Exception):
+    '''
+    Exception for sensor initialisation errors.
+    '''
+    pass
+
+
+class SensorMeasurementError(Exception):
+    '''
+    Exception for sensor measurement errors.
+    '''
+    pass
 
 
 class SensorType(Enum):
@@ -30,21 +45,60 @@ class DS18B20Sensor(AbstractSensor):
     '''
     A sensor that measures the temperature of a DS18B20 sensor.
     '''
-    def __init__(self, sensor_id: str):
+
+    type = SensorType.TEMPERATURE
+
+    def __init__(self, sensor_id: str, device_file: Path):
         '''
         Initializes a new DS18B20Sensor.
+
+        :param sensor_id: sensor id
+        :param device_file: path to the device file
+
         '''
         self.sensor_id = sensor_id
-        self.type = SensorType.TEMPERATURE
+        self.device_file = device_file
+
+        if not device_file.is_file():
+            raise SensorInitError('Device file does not exist')
+
+    def _read_device_file(self):
+        with open(self.device_file, 'r') as f:
+            lines = f.readlines()
+        return lines
 
     def get_measurement(self) -> Measurement:
         '''
         Returns the current measurement.
+
+        :return: a measurement
+        raises: SensorMeasurementError
         '''
-        return Measurement(self.sensor_id, get_timestamp_now(), 1.0)
+        raw_measurement = self._read_device_file()
+        if raw_measurement[0].strip()[-3:] != 'YES':  # check CRC
+            raise SensorMeasurementError(
+                'Error reading sensor [{}] from device file [{}] bad CRC'.format(self.sensor_id, self.device_file)
+                )
+
+        equals_pos = raw_measurement[1].find('t=')  # find temperature
+        if equals_pos == -1:
+            raise SensorMeasurementError(
+                'Error reading sensor [{}] from device file [{}] cannot determine temperature value'.format(self.sensor_id, self.device_file)
+                )
+
+        temp_string = raw_measurement[1][equals_pos+2:]
+
+        try:
+            temp_c = round(float(temp_string) / 1000.0, 1)  # convert to celsius
+        except ValueError:
+            raise SensorMeasurementError(
+                'Error reading sensor [{}] from device file [{}] cannot convert [{}] to celcius'.format(self.sensor_id, self.device_file, temp_string)
+                )
+
+        return Measurement(self.sensor_id, get_timestamp_now(), temp_c)
 
 
-def get_timestamp_now():
+def get_timestamp_now() -> float:
     '''
     Returns the current timestamp.
     '''
--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -1,15 +1,87 @@
-from monitor.sensors import DS18B20Sensor, SensorType
+from pathlib import Path
+
+import pytest
+
+from monitor.sensors import DS18B20Sensor, SensorType, SensorInitError, SensorMeasurementError
 
 
-def test_ds18b20_sensor_init():
-    s = DS18B20Sensor('sensor_id')
+def test_ds18b20_sensor_init(tmpdir):
+    device_file = Path(tmpdir) / 'sensor.txt'
+    device_file.touch()
+    s = DS18B20Sensor('sensor_id', device_file)
     assert s.sensor_id == 'sensor_id'
     assert s.type == SensorType.TEMPERATURE
 
 
-def test_ds18b20_sensor_get_measurement(timestamp_fixture):
-    s = DS18B20Sensor('sensor_id')
+def test_ds18b20_sensor_init_bad_device_file(tmpdir):
+    device_file = Path(tmpdir) / 'sensor.txt'
+    with pytest.raises(SensorInitError):
+        DS18B20Sensor('sensor_id', device_file)
+
+
+@pytest.fixture
+def good_measurement():
+    return '''bd 00 4b 46 ff ff ff ff ff ff : crc=ff YES
+    bd 00 4b 46 ff ff ff ff ff ff t=27772'''
+
+
+def test_ds18b20_sensor_get_measurement(tmpdir, timestamp_fixture, good_measurement):
+    device_file = Path(tmpdir) / 'sensor.txt'
+    device_file.write_text(good_measurement)
+    s = DS18B20Sensor('sensor_id', device_file)
     m = s.get_measurement()
     assert m.sensor_id == 'sensor_id'
     assert m.timestamp == timestamp_fixture
-    assert m.value == 1.0
+    assert m.value == 27.8
+
+
+@pytest.fixture
+def bad_crc():
+    return '''bd 00 4b 46 ff ff ff ff ff ff : crc=ff NO
+    bd 00 4b 46 ff ff ff ff ff ff t=27812'''
+
+
+def test_ds18b20_sensor_get_measurement_bad_crc(tmpdir, bad_crc):
+    device_file = Path(tmpdir) / 'sensor.txt'
+    device_file.write_text(bad_crc)
+    s = DS18B20Sensor('sensor_id', device_file)
+    with pytest.raises(SensorMeasurementError):
+        s.get_measurement()
+
+
+@pytest.fixture
+def missing_t_measurement():
+    return '''bd 00 4b 46 ff ff ff ff ff ff : crc=ff YES
+    bd 00 4b 46 ff ff ff ff ff ff '''
+
+
+def test_ds18b20_sensor_get_measurement_missing_t_measurement(tmpdir, missing_t_measurement):
+    device_file = Path(tmpdir) / 'sensor.txt'
+    device_file.write_text(missing_t_measurement)
+    s = DS18B20Sensor('sensor_id', device_file)
+    with pytest.raises(SensorMeasurementError):
+        s.get_measurement()
+
+
+@pytest.fixture
+def not_a_number_measurement():
+    return '''bd 00 4b 46 ff ff ff ff ff ff : crc=ff YES
+    bd 00 4b 46 ff ff ff ff ff ff t=278a2'''
+
+
+def test_ds18b20_sensor_get_measurement_not_a_number_measurement(tmpdir, not_a_number_measurement):
+    device_file = Path(tmpdir) / 'sensor.txt'
+    device_file.write_text(not_a_number_measurement)
+    s = DS18B20Sensor('sensor_id', device_file)
+    with pytest.raises(SensorMeasurementError):
+        s.get_measurement()
+
+
+def test_ds18b20_sensor_get_measurement_rounds_to_one_decimal(tmpdir, timestamp_fixture, good_measurement):
+    device_file = Path(tmpdir) / 'sensor.txt'
+    device_file.write_text(good_measurement)
+    s = DS18B20Sensor('sensor_id', device_file)
+    m = s.get_measurement()
+    assert m.sensor_id == 'sensor_id'
+    assert m.timestamp == timestamp_fixture
+    assert m.value == 27.8
